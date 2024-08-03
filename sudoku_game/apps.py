4280edd1523b4743de6499c37b7f76cadb58b2de
Aijaisarma Sabaratnasarma <Aijaysarma14@gmail.com>
2024-08-03 18:18:58 -0400

    Removed old sudoku game folder

--- sudoku_game/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SudokuGameConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "sudoku_game"
--- sudoku_game/server/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from sudoku_game.server import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
